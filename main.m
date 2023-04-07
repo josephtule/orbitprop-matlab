ddb858af3488da91d8c75260330e3f2a76e0a501
Joseph Le <54126483+josephtule@users.noreply.github.com>
2023-04-06 23:45:00 -0600

    Update main.m
    clean up

--- main.m
+++ main.m
@@ -1,16 +1,18 @@
 clear all; close all;
 e = earth();
-% rp = e.radius+250;
-% ecc = 0; i = deg2rad(80); a = rp/(1-ecc);
+% Hyperbolic Orbit with Inclination Example
+% rp = e.radius+500;
+% ecc = 1.2; i = deg2rad(25); a = rp/(1-ecc);
 % RAAN = 0; AOP = 0; TA = 0;
 
 
 % ISS Example
 a = 8059.643886; ecc = 0.17141769; i = deg2rad(27.99996);
 RAAN = deg2rad(45.00000175); AOP = deg2rad(30.0127649); TA = deg2rad(39.98711236);
-
 coes = [a,ecc,i,RAAN,AOP,TA].'; % [a,e,i,RAAN,AOP,TA]
-% tend = 2*pi/sqrt(e.mu)*(abs(a))^(3/2);
+
+
+% tend = 2*pi/sqrt(e.mu)*(abs(a))^(3/2); % period time
 tend = 24*60*60 * 2;
 config_test = struct('dt',200, ...
     'tspan',[0,tend], ...
@@ -19,10 +21,11 @@ config_test = struct('dt',200, ...
     'perts',"j2", ...
     'calc_coes',1, ...
     'drawopt',1);
-sc1 = spacecraft(config_test);
-state0 = sc1.state(:,1);
+sc1 = spacecraft(config_test); % cool stuff done here
 
+% plot orbit
 figure(1)
+state0 = sc1.state(:,1);
 plot3(sc1.state(1,:),sc1.state(2,:),sc1.state(3,:))
 hold on
 plot3(state0(1),state0(2),state0(3),"*")
@@ -32,6 +35,7 @@ axis equal
 xlabel('x'),ylabel('y'),zlabel('z')
 grid on
 
+% plot classical orbital elements vs time since start
 figure(2)
 t = linspace(0,24*60*60 * 2,size(sc1.state,2));
 titles = ["a","e","i","\Omega","\omega","\theta"];
@@ -47,3 +51,6 @@ for k = 1:size(sc1.coes,1)
     title(titles(k))
     grid on
 end
+
+
+
