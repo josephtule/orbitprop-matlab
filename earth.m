c5fdde29e8aa5f50db34c313b84d613c42fcf8bf
Joseph Le <54126483+josephtule@users.noreply.github.com>
2023-04-06 23:16:52 -0600

    Update earth.m
    added j2

--- earth.m
+++ earth.m
@@ -1,7 +1,10 @@
 classdef earth < planet
     methods
         function e = earth()
-            e@planet("earth",6378.14,3.986004418e5);
+            e@planet("earth", ... % name
+                     6378.14, ... % radius
+                     3.986004418e5, ... % mu
+                     1.082635854e-3); % j2
         end
     end
 end
