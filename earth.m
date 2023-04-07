b2b42e1ad360354c62a0f050ae4510a7dfc3df0b
Joseph Le <54126483+josephtule@users.noreply.github.com>
2023-04-07 15:58:15 -0600

    Update earth.m

--- earth.m
+++ earth.m
@@ -4,7 +4,10 @@ classdef earth < planet
             e@planet("earth", ... % name
                      6378.14, ... % radius
                      3.986004418e5, ... % mu
-                     1.082635854e-3); % j2
+                     1.082635854e-3, ... % j2
+                     [63.096 251.189 1000; 2.059e-4 5.909e-11 3.561e-15], ... % rhos, [altitude, density]
+                     [0;0;72.9211e-6] ... % rotation vector
+                     );
         end
     end
 end
