0419de3b6eaae137b731dd1d8230aa715df4de32
Joseph Le <54126483+josephtule@users.noreply.github.com>
2023-04-07 15:58:03 -0600

    Update planet.m

--- planet.m
+++ planet.m
@@ -3,15 +3,19 @@ classdef planet
         name
         radius
         mu
-        j2
+        j2 = 0
+        rhos = 0;
+        atmos_rot = 0;
     end
 
     methods
-        function pl = planet(name,radius,mu,j2)
+        function pl = planet(name,radius,mu,j2,rhos,atmos_rot)
             pl.name = name;
             pl.radius = radius;
             pl.mu = mu;
             pl.j2 = j2;
+            pl.rhos = rhos;
+            pl.atmos_rot = atmos_rot;
         end
 
         function plotplanet(pl,offset)
